CyanogenMod/android_device_htc_m8

init: build as C++
Change-Id: I032c6c42df6d9c01217b05f7ee69eea252aa8788

init/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_m8.c
+LOCAL_SRC_FILES := init_m8.cpp
 LOCAL_MODULE := libinit_m8
 include $(BUILD_STATIC_LIBRARY)

sub77/android_device_htc_m8qlul

init/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_m8qlul.c
+LOCAL_SRC_FILES := init_m8qlul.cpp
 LOCAL_MODULE := libinit_m8qlul
 include $(BUILD_STATIC_LIBRARY)
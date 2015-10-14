138dd60a291ec46ecdacca667048f95e276ad947
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-14 15:03:16 -0500

    init: build as C++
    Change-Id: I032c6c42df6d9c01217b05f7ee69eea252aa8788

--- init/Android.mk
+++ init/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_m8qlul.c
+LOCAL_SRC_FILES := init_m8qlul.cpp
 LOCAL_MODULE := libinit_m8qlul
 include $(BUILD_STATIC_LIBRARY)
